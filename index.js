b9334b0e794b9519d6d41959dd4eb9f8bba1f8c6
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2023-03-03 21:44:54 +0530

    update

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 let buttons = document.querySelectorAll(".btn")
 let input = document.querySelector("#input")
-console.log(buttons.value)
+// console.log(buttons.value)
+let str = ""
 
-function addValue(e){
-    input.value += e
-}
-function calculate(){
-    input.value = eval(input.value)
-}
-function Reset(){
-    input.value = ""
-}
+Array.from(buttons).forEach((buttons) => {
+    buttons.addEventListener("click", (e) => {
+        // console.log(e.target.value)
+        if (e.target.value == "C") {
+            str = ""
+            input.value = str
+        } else if (e.target.value == "=") {
+            if (str == "") {
+                return
+            }
+            str = eval(str)
+            console.log(str)
+            input.value = str
+        } else {
+            if ((e.target.value == "+" || e.target.value == "-" || e.target.value == "*" || e.target.value == "/" || e.target.value == ".") && (str[str.length - 1] == "+" || str[str.length - 1] == "-" || str[str.length - 1] == "*" || str[str.length - 1] == "/" || str[str.length - 1] == ".")) {
+                return
+            }
+            if(str == "" && (e.target.value == "." || e.target.value == "/" || e.target.value == "*")){
+                input.value = str
+                return
+            }
+            str += e.target.value
+            input.value = str
+        }
+    })
+})
