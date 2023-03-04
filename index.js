90e4d1af90f831181294887de981336a40446546
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2023-03-04 15:41:43 +0530

    update

--- index.js
+++ index.js
@@ -1,36 +1,79 @@
 let buttons = document.querySelectorAll(".btn")
 let input = document.querySelector("#input")
 // console.log(buttons.value)
+let toggle = true
 let str = ""
-
-Array.from(buttons).forEach((buttons) => {
-    buttons.addEventListener("click", (e) => {
-        // console.log(e.target.value)
-        if (e.target.value == "C") {
+Array.from(buttons).forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        let val = e.target.value
+        if (val == "C") {
             str = ""
             input.value = str
-        }else if(e.target.value == "Del") {
-            str = str.toString()
-            str = str.slice(0, str.length-1)
-            input.value = str
-        }
-        else if (e.target.value == "=") {
-            if (str == "") {
+        }else if (val == "=") {
+            if(str == ""){
                 return
             }
             str = eval(str)
-            console.log(str)
             input.value = str
-        } else {
-            if ((e.target.value == "+" || e.target.value == "-" || e.target.value == "*" || e.target.value == "/" || e.target.value == ".") && (str[str.length - 1] == "+" || str[str.length - 1] == "-" || str[str.length - 1] == "*" || str[str.length - 1] == "/" || str[str.length - 1] == ".")) {
-                return
+            if (str.toString().includes(".")) {
+                console.log("hh")
+                toggle = false
+            } else {
+                toggle = true
             }
-            if(str == "" && (e.target.value == "." || e.target.value == "/" || e.target.value == "*")){
+        }else if(val == "Del"){
+            str = str.toString()
+            str = str.slice(0, str.length-1)
+            input.value = str
+        }else {
+            if (val == ".") {
+                console.log("hu")
+                console.log(toggle)
+                if (toggle === true) {
+                    console.log(str[str.length-1])
+                    if ((str[str.length - 1] == "+" || str[str.length - 1] == "-" || 
+                    str[str.length - 1] == "*" ||
+                     str[str.length - 1] == "/" ||
+                      str[str.length - 1] == ".") && (val == "+" || val == "-" || val == "*" || val == "/" || val == ".")) {
+                        // console.log("val")
+                        return
+                    }
+                    if (str == "" && (val == "/" || val == "*" || val == ".")) {
+                        input.value = str
+                        return;
+                    }
+                    str += val
+                    input.value = str
+                    toggle = false
+
+                } else {
+                    return
+                }
+
+            }else if(["+", "/", "*", "-"].includes(val)){
+                if ((str[str.length - 1] == "+" || str[str.length - 1] == "-" || str[str.length - 1] == "*" || str[str.length - 1] == "/" || str[str.length - 1] == ".") && (val == "+" || val == "-" || val == "*" || val == "/" || val == ".")) {
+                    return
+                }
+                if (str == "" &&(val == "/" || val == "*" || val == ".")) {
+                    input.value = str
+                    return;
+                }
+                str += val
                 input.value = str
-                return
+                toggle = true
+            }else{
+                console.log(str[str.length-1])
+                if ((str[str.length - 1] == "+" || str[str.length - 1] == "-" || str[str.length - 1] == "*" || str[str.length - 1] == "/" || str[str.length - 1] == ".") && (val == "+" || val == "-" || val == "*" || val == "/" || val == ".")) {
+                    return
+                }
+                if (str == "" && (val == "/" || val == "*" || val == ".")) {
+                    input.value = str
+                    return;
+                }
+                str += val
+                input.value = str
+            
             }
-            str += e.target.value
-            input.value = str
         }
     })
 })
